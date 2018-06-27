d83b989080f93c21ad9380189a6bc36b7c9ada88
DANIEL <daniel2242014@gmail.com>
2018-06-27 13:11:59 -0300

    Declaracion de los metodos y variables de Jugador y Acciones

--- src/logica/Acciones.java
+++ src/logica/Acciones.java
@@ -1,5 +1,124 @@
 package logica;
-
+import java.util.ArrayList;
 public class Acciones {
+  
+    private Jugador personaje;
+    private int amarilias, cant_goles, faltasTotales;
+    private static final int TOTAL=2;
+    private static final int PARCIAL=1;
+    private static final int NULA=0;
+    private boolean roja, estadoInicial, estadoFinal;
+    private double tiempo_actualizacion;
+    private ArrayList <Boolean> penales = new ArrayList();
+    private ArrayList <String> momento_goles = new ArrayList();
+    
+    public Acciones(Jugador g){
+        this.personaje=g;
+    }
+
+    public Jugador getPersonaje() {
+        return personaje;
+    }
+
+    public void setPersonaje(Jugador personaje) {
+        this.personaje = personaje;
+    }
+
+    public int getAmarilias() {
+        return amarilias;
+    }
+
+    public void setAmarilias(int amarilias) {
+        this.amarilias = amarilias;
+    }
+
+    public int getCant_goles() {
+        return cant_goles;
+    }
+
+    public void setCant_goles(int cant_goles) {
+        this.cant_goles = cant_goles;
+    }
+
+    public int getFaltasTotales() {
+        return faltasTotales;
+    }
+
+    public void setFaltasTotales(int faltasTotales) {
+        this.faltasTotales = faltasTotales;
+    }
+
+    public boolean isRoja() {
+        return roja;
+    }
+
+    public void setRoja(boolean roja) {
+        this.roja = roja;
+    }
+
+    public boolean isEstadoInicial() {
+        return estadoInicial;
+    }
+
+    public void setEstadoInicial(boolean estadoInicial) {
+        this.estadoInicial = estadoInicial;
+    }
+
+    public boolean isEstadoFinal() {
+        return estadoFinal;
+    }
+
+    public void setEstadoFinal(boolean estadoFinal) {
+        this.estadoFinal = estadoFinal;
+    }
+
+    public double getTiempo_actualizacion() {
+        return tiempo_actualizacion;
+    }
+
+    public void setTiempo_actualizacion(double tiempo_actualizacion) {
+        this.tiempo_actualizacion = tiempo_actualizacion;
+    }
+
+    public ArrayList<Boolean> getPenales() {
+        return penales;
+    }
+
+    public void setPenales(ArrayList<Boolean> penales) {
+        this.penales = penales;
+    }
+
+    public ArrayList<String> getMomento_goles() {
+        return momento_goles;
+    }
+
+    public void setMomento_goles(ArrayList<String> momento_goles) {
+        this.momento_goles = momento_goles;
+    }
+    
+    public void cambio(boolean t,String tiempo){
+    //  falta    
+    }
+    
+    public void falta(int numero){
+        // falta
+    }
+    
+    public void gol(String momento){
+        //falta
+    }
+    
+    public void penal (boolean j){
+        //falta
+    }
+
+    @Override
+    public String toString() {
+        return "Acciones{" + "personaje=" + personaje + ", amarilias=" + amarilias + ", cant_goles=" +
+                cant_goles + ", faltasTotales=" + faltasTotales + ", roja=" + roja + ", estadoInicial=" + 
+                estadoInicial + ", estadoFinal=" + estadoFinal + ", tiempo_actualizacion=" + 
+                tiempo_actualizacion + ", penales=" + penales + ", momento_goles=" + momento_goles + '}';
+    }
+    
     
 }
--- src/logica/Jugador.java
+++ src/logica/Jugador.java
@@ -2,11 +2,141 @@ package logica;
 
 
 public class Jugador {
-    private int edad;
-    private String cedula;
-    private String nombre;
-    public Jugador(){
-        
+    private String primeraPosicion,segundaPosicion,fechaDebut,apellido,club,
+            cedula,nombre,fechaNacimiento,pais;
+    private int edad,golesTotales,faltasTotales;
+    private double altura;
+  
+    public Jugador(String nom,String apeli, String pai,String code ){
+        this.nombre=nom;
+        this.apellido=apeli;
+        this.cedula=code;
+        this.pais=pai;
     }
+
+    public String getPrimeraPosicion() {
+        return primeraPosicion;
+    }
+
+    public void setPrimeraPosicion(String primeraPosicion) {
+        this.primeraPosicion = primeraPosicion;
+    }
+
+    public String getSegundaPosicion() {
+        return segundaPosicion;
+    }
+
+    public void setSegundaPosicion(String segundaPosicion) {
+        this.segundaPosicion = segundaPosicion;
+    }
+
+    public String getFechaDebut() {
+        return fechaDebut;
+    }
+
+    public void setFechaDebut(String fechaDebut) {
+        this.fechaDebut = fechaDebut;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getClub() {
+        return club;
+    }
+
+    public void setClub(String club) {
+        this.club = club;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getGolesTotales() {
+        return golesTotales;
+    }
+
+    public void setGolesTotales(int golesTotales) {
+        this.golesTotales = golesTotales;
+    }
+
+    public int getFaltasTotales() {
+        return faltasTotales;
+    }
+
+    public void setFaltasTotales(int faltasTotales) {
+        this.faltasTotales = faltasTotales;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+    
+    
+    
+    public void agregar_gol(int numero){
+        /*FALTA COMPLETAR*/
+    }
+    
+    public void agregar_falta(int numero){
+        /*FALTA COMPLETAR*/
+    }
+
+    @Override
+    public String toString() {
+        return "Jugador{" + "primeraPosicion=" + primeraPosicion + ", segundaPosicion=" + segundaPosicion 
+                +  ", fechaDebut=" + fechaDebut + ", apellido=" + apellido + ", club=" + club + ", cedula=" + cedula +
+                ", nombre=" + nombre + ", fechaNacimiento=" + fechaNacimiento + ", pais=" + pais + ", edad="
+                + edad + ", golesTotales=" + golesTotales + ", faltasTotales=" 
+                + faltasTotales + ", altura=" + altura + '}';
+    }
+    
+    
     
 }
