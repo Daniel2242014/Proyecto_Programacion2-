2f3edac36c9324b32882ce48cac66b8e06ee58d3
Agustin <agus-102@hotmail.com>
2018-06-26 23:21:25 -0300

    aaaaaaaa

--- src/logica/Extra.java
+++ src/logica/Extra.java
@@ -1,10 +1,34 @@
 package logica;
 
-//PRUEBAAAAAAA DANI COMUNISTA
-public class Extra 
+import java.util.ArrayList;
+
+public class Extra extends Acciones
 {
-    Extra ()
+    private ArrayList<Acciones> eventos=new ArrayList<>();
+    private int terminar;
+    private ArrayList<Jugador> tiradores=new ArrayList<>();
+    public static int TERMINAR_PRIMER_TIEMPO;
+    public static int TERMINAR_PENALES;
+           
+    public Extra (int a, int b, boolean bool, ArrayList<Acciones> arr)
     {
         
     }
+    
+    public void penal(Jugador p, boolean bool, int n, double nn)
+    {
+        
+    }
+    
+    public boolean devolverPenales()
+    {
+        return false;
+    }
+
+    @Override
+    public String toString() 
+    {
+        return "Extra{" + "eventos=" + eventos + ", terminar=" + terminar + ", tiradores=" + tiradores + '}';
+    }
+    
 }
