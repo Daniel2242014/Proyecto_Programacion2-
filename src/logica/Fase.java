74aab4e897084e187e0d7ced787031037d3ed492
DANIEL <daniel2242014@gmail.com>
2018-07-05 15:18:37 -0300

    correcion de errores y comentarios

--- src/logica/Fase.java
+++ src/logica/Fase.java
@@ -1,117 +1,72 @@
-/* ANTES
-package logica;
-
-
-    public class Face { /*"FACE" CON "S" NO CON "C"
-                                               DANIEL*/
-        private partidos : ArrayList<Partido>;  //no se que falta
-        
-        /*CUIDADO CON LA SINTAXIS, NO SON : SINO UN  =,
-        DANIEL */
-        
-        private String letra_posicion;
-        private boolean grupo_eliminacion;
-        
-        
-        public void Fase(){ //no se si tiene que ir void o no.
-           
-        }
-        public void agregarPartido(Partido){ // ni idea de que esta mal
-            return null;
-       
-        /*SI ES VOID NO LLEVA RETURN, ADEMAS NO TE OLVIDES DE PONERLE UN NOMBRE A LOS PARAMETOS 
-            NO SOLO EL TIPO DE DATO 
-            DANIEL */
-        }
-       
-        
-        public Partido devolverPartido(int){  //no se que falta
-            return null;
-            
-            /*NOMBRE EN LOS TIPO DE PARAMETO 
-            DANIEL */
-            
-        }
-        public int cantidadPartidos(){
-            
-        }
-        public Selecion[] ganadores(){  //no se que falta
-            return null;
-            /*PARA ELIMINAR LOS ERRORES DEBERIAS CREAR UNA VARIABLE FICTICIA, NO DEBUELVAS NULL
-            DANIEL*/
-        }
-        public Selecion[] devolverSelecciones(){  //no se que falta
-            return null;
-            /*LO MISMO QUE LO ANTERIOR
-            DANIEL*/
-        }
-        public int devolverPunto(Selecion){  //no se que falta
-            return null;  //no se que falta
-            /*NOMBRE DE LOS PARAMETOS Y NO DEBUELVAS NULL
-            DANIEL*/
-        }
-        public int devolverGoles(Selecion){  //no se que falta
-            return null;  //no se que falta
-             /*NOMBRE DE LOS PARAMETOS Y NO DEBUELVAS NULL
-            DANIEL*/
-        }
-        public String toString() {  
-            return "hola"; //LE PUSE "HOLA" COMO EJEMPLO
-            
-            /*DEJA QUE NETBEANS LO HAGA 
-            DANIEL*/
-        }
-        
-        
-    
-                    
-    }
-  */ 
-      
-// ahora
 package logica;
 
     public class Fase { 
-        private partidos = ArrayList<Partido>; //sigue tirando error
+        private Partidos /*EL NOMBRE*/ = ArrayList<Partido>;  /*ArrayList <Partido> nombre = new ArrayList<>(); DANIEL*/
         private String letra_posicion;
         private boolean grupo_eliminacion;
-        public int eje;
+        public int eje; /*¿CUAL ES ESTA VARIABLE?*/
         
         public void Fase(){ 
            
         }
         public void agregarPartido(Partido part){ 
-       
-       
+                /*AGREGA UNA UN OBJETO DE TIPO PARTIDO (EL QUE TENES EN EL PARAMETRO DEL METODO) 
+                        AL ARRAYLIST, UTILIZA EL METODO ADD (PARTIDO)
+                        DANIEL*/
         }
        
         
         public Partido devolverPartido(int devpart){  
             return null;
             //devpart=devolverPartido
+            /*UTILIZA LOS METODOS DE ARRAYLIST PARA DEVOLVE EL PARTIDO QUE ESTE EN EL ARRAYLIST
+            QUE CONCUERDE CON LA POSICION DEL INT DEL PARAMETRO, USA EL METODO GET(INT)
+            DANIEL*/
             
         }
         public int cantidadPartidos(){
-            
+            /*DEVOLVE LA CANTIDAD DE ELEMENTOS QUE TENGA EL ARRAYLIST (UTILIZA EL METODO SIZE()) */
+            return 0;
         }
-        public Seleccion[] ganadores(){  //no se que falta
-            return eje;
+        public Selecion[] ganadores(){  //no se que falta
+            return null; 
+            /*EHH? QUIEN ES EJE Y ADEMAS UN INT NO ES UNA SELECION
+             DANIEL*/
+             
+            /*ESTE METODO LO QUE HACE ES DEVOLBER UN ARRAY DE LAS SELECIONES QUE
+                 GANARON ESA FACE, UTILIZA LOS METODOS DE PARTIDO PARA CALCULAR LOS PUNTOS 
+                 Y GOLES, CON ESO SACAS QUIENES GANARON LA FACE (NO IMPORTA QUE AUN NO 
+                 TENGAS LO VALORES, SOLO IMPORTA EL ALGORITMO).
+                 PRIMERO SACA QUIENES SON LAS SELECIONES, TE ACONSEJO CREAR OTRO METODO PARA ESO.
+                 DANIEL */
            
         }
-        public Seleccion[] devolverSelecciones(){  //no se que falta
-            return eje;
+        public Selecion[] devolverSelecciones(){  //no se que falta
+            return null;
+            
+            /*LO MISMO QUE EL ANTERIOR, PERO SOLO QUE ESTE DEVUELBE LOS QUE NO GANARON, 
+            IGUAL PARA OPTIMISAR CODIGO PODES UTILIZAR EL OTRO METODO Y VER CUALES SON 
+            LAS SELECIONES QUE NO ESTAN  */
+                
         
         }
-        public int devolverPunto(Seleccion a){ 
-            return eje;
+        public int devolverPunto(Selecion a){ 
+            return 0;
           
+            /*TIENE QUE DEVOLBER, LOS PUNTOS QUE TUBO LA SELECION ES ESA FACE, TE PUEDE SERVIR 
+            REUTILIZAR ESTOS METODOS (ESTE Y EL SIGIENTE) EN LOS DOS ANTERIORES
+            DANIEL*/
+            
         }
-        public int devolverGoles(Seleccion a){  
-            return eje;  
+        public int devolverGoles(Selecion a){  
+            return 0;  
+            
+            /*TIENE QUE DEVOLBER LOS GOLES QUE HISO LA SELECION EN ESA FACE */
             
         }
         public String toString() {
-            return "Fase{" + "letra_posicion=" + letra_posicion + ", grupo_eliminacion=" + grupo_eliminacion + ", eje=" + eje + '}';
+            return "Fase{" + "letra_posicion=" + letra_posicion + ", grupo_eliminacion=" + 
+                    grupo_eliminacion + ", eje=" + eje + '}';
             //deje que lo haga netbeans como dijiste
         
         }
--- src/logica/Partido.java
+++ src/logica/Partido.java
@@ -1,95 +1,57 @@
-/* ANTES
 package logica;
-
+/*IMPORTA LA CLASE ARRAYLIST, ES DEL PAQUETE JAVA.UTIL, SINO NO ANDA */
 public class Partido {
     private String fecha;
     private int tiempo_total;
     private int tiempo_agregado;
     private String ciudad;
     private String estadio;
-    private Selecion equipo1;          //no
-    private Seleecion equipo2;          //se
-    private ArrayList<Accion> eventos;  //que
-    private Extra parte_extra;          //pija esta mal
-    private boolean tipo_frase;
-    
-    /*ESCRIBI BIEN LOS NOMBRE DE LAS CLASES; MIRA COMO ESTAN ESCRITAS A TU 
-    DANIEL*/
-    
-    
-    public  Partido(Selecion,Selecion,boolean,int){  //no se que falta
-        return null;
-        /*EL CONSTRUCTOR NO LLEVA RETURN Y PONE NOMBRE A LOS PARAMETOS
-        DANIEL*/
-    }
-    public void agregarEvento(Accion){  //no se que falta
-        return null;
-        /*NO RETURN, NOMBRE A LOS PARAMETROS
-        DANIEL*/
-    } 
-    public Accion devolverEvento(int){  //no se que falta
-        return null;
-        /*NOMBRE A LOS PARAMETROS
-        DANIEL*/
-    }
-    public int cantidadEventos(){
-        return 4; //ejemplo
-    }
-    public void cearAlargue(){ //LE PUSE VOID PORQUE SINO TIRA ERROR.
-        
-    }
-    public int golesTotales(Selecion){  //no se que falta
-        return 4; //LE PUSE 4 COMO EJEMPLO SINO ME TIRABA ERROR.
-    }
-    public String toString(){
-        return "hola"; //LE PUSE "HOLA" COMO EJEMPLO SINO ME TIRABA ERROR.
-        /*DEJA QUE LO CREE NETBEANDS
-        DANIEL*/
-    }
-        
-    
-    
-           
-}
-*/
-//AHORA
-package logica;
-
-public class Partido {
-    private String fecha;
-    private int tiempo_total;
-    private int tiempo_agregado;
-    private String ciudad;
-    private String estadio;
-    private Seleccion equipo1;    
-    private Seleeccion equipo2;     
-    private ArrayList<Accion> eventos;  
+    private Selecion equipo1;    
+    private Selecion equipo2;     
+    private ArrayList<Acciones> eventos;  
     private Extra parte_extra;          
     private boolean tipo_fase;
     
     
-    public  Partido(Seleccion a,Seleccion b,boolean fac,int temptot){
+    public  Partido(Selecion a,Selecion b,boolean fac,int temptot){
         //fac=tipo_fase, temptot=tiempo_total
+        /*ASIGNA LOS PARAMETOS A LAS VARIABLES CORESPONDIENTES (EJ: equipo1=a)
+        DANIEL*/
     }
-    public void agregarEvento(Accion acc){  
+    public void agregarEvento(Acciones acc){  
       //acc=accion  
-      
+      /*TENES QUE AGREGAR LA ACCION QUE TE MANDARON POR PARAMETRO Y AGREGARLA 
+      AL ARRAYLIST, UTILIZA EL METODO ADD(ACCIONES)
+      DANIEL*/
     } 
-    public Accion devolverEvento(int deveve){  
+    public Acciones devolverEvento(int deveve){  
         return null;
         //deveve=devolverEvento
+        /*UTILIZANDO EL METODO GET(INT) DEVOLBE LA ACCION QUE CORESPONDA AL NUMERO QUE TE
+        PASARON POR PARAMETRO
+        DANIEL*/
     }
     public int cantidadEventos(){
         return 4; 
+        /*UTILIZA EL METODO SIZE() PARA SABER Y DEBOLVER CUANTAS ACCIONES HAY EN EL ARRAYLIST
+        DANIEL*/
     }
     public void cearAlargue(){ 
-        
+        /*CREA UN OBJETO DE TIPO EXTRA Y PASALE LAS COSAS CORESPONDIENTES AL CONSTRUCTOR
+        DANIEL*/
     }
-    public int golesTotales(Seleccion){  
+    public int golesTotales(Selecion /*NOMBRE*/){  
         return 4; 
+        /*MIRANDO EN LAS ACCIONES CALCULA CUANTOS FUERON LOS GOLES TOTALES DE CADA SELECION 
+        EN EL PARTIDO, RECORDA QUE SABES CUALES SON LOS JUGADORES DE CADA SELECION, PORQUE LOS 
+        TENES METIDOS EN UN ARRAYLIST EN LA CLASE SELECION
+        DANIEL*/
     }
     public String toString() {
-        return "Partido{" + "fecha=" + fecha + ", tiempo_total=" + tiempo_total + ", tiempo_agregado=" + tiempo_agregado + ", ciudad=" + ciudad + ", estadio=" + estadio + ", equipo1=" + equipo1 + ", equipo2=" + equipo2 + ", eventos=" + eventos + ", parte_extra=" + parte_extra + ", tipo_fase=" + tipo_fase + '}';
+        return "Partido{" + "fecha=" + fecha + ", tiempo_total=" + tiempo_total + ", tiempo_agregado=" 
+                + tiempo_agregado + ", ciudad=" + ciudad + ", estadio=" + estadio + ", equipo1=" + equipo1
+                + ", equipo2=" + equipo2 + ", eventos=" + eventos + ", parte_extra=" + parte_extra
+                + ", tipo_fase=" + tipo_fase + '}';
     } //deje que lo hiciera netbeans como dijiste
        
     
