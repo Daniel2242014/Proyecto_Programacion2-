1445adfa3242d1b897ec0da235c6add79daebb78
mrfacundo3000 <40530248+mrfacundo3000@users.noreply.github.com>
2018-07-06 18:21:17 +0000

    AIIIIIIIIIIIIIUUUUUUUUUUUUUUUUDAAAAAAAAAAAAA

--- src/logica/Partido.java
+++ src/logica/Partido.java
@@ -1,5 +1,5 @@
 package logica;
-/*IMPORTA LA CLASE ARRAYLIST, ES DEL PAQUETE JAVA.UTIL, SINO NO ANDA */
+import.java.util;
 public class Partido {
     private String fecha;
     private int tiempo_total;
@@ -15,35 +15,40 @@ public class Partido {
     
     public  Partido(Selecion a,Selecion b,boolean fac,int temptot){
         //fac=tipo_fase, temptot=tiempo_total
-        /*ASIGNA LOS PARAMETOS A LAS VARIABLES CORESPONDIENTES (EJ: equipo1=a)
-        DANIEL*/
+        equipo1=a;
+        equipo2=b;
+        tipfase=fac;
+        timetotal=temptot;
+ 
     }
     public void agregarEvento(Acciones acc){  
       //acc=accion  
-      /*TENES QUE AGREGAR LA ACCION QUE TE MANDARON POR PARAMETRO Y AGREGARLA 
+      eventos.add(Acciones);
+        /*TENES QUE AGREGAR LA ACCION QUE TE MANDARON POR PARAMETRO Y AGREGARLA 
       AL ARRAYLIST, UTILIZA EL METODO ADD(ACCIONES)
       DANIEL*/
     } 
     
     public Acciones devolverEvento(int deveve){  
-        return null;
-
-        //deveve=devolverEvento
-        /*UTILIZANDO EL METODO GET(INT) DEVOLBE LA ACCION QUE CORESPONDA AL NUMERO QUE TE
+        getInt()=a;
+        return eventos.get(a);
+            
+            /*UTILIZANDO EL METODO GET(INT) DEVOLBE LA ACCION QUE CORESPONDA AL NUMERO QUE TE
         PASARON POR PARAMETRO
         DANIEL*/
     }
     public int cantidadEventos(){
-        return 4; 
+        return eventos.size(); 
         /*UTILIZA EL METODO SIZE() PARA SABER Y DEBOLVER CUANTAS ACCIONES HAY EN EL ARRAYLIST
         DANIEL*/
     }
     public void cearAlargue(){ 
+        //como es?
         /*CREA UN OBJETO DE TIPO EXTRA Y PASALE LAS COSAS CORESPONDIENTES AL CONSTRUCTOR
         DANIEL*/
     }
     public int golesTotales(Selecion /*NOMBRE*/){  
-        return 4; 
+        for ( eventos : //no me acuerdo que iba aca para correr toda la lista
         /*MIRANDO EN LAS ACCIONES CALCULA CUANTOS FUERON LOS GOLES TOTALES DE CADA SELECION 
         EN EL PARTIDO, RECORDA QUE SABES CUALES SON LOS JUGADORES DE CADA SELECION, PORQUE LOS 
         TENES METIDOS EN UN ARRAYLIST EN LA CLASE SELECION
