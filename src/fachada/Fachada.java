7c61d79e1e7e54045167110fb14aa654fdcc4a69
Facu <facundosilvetti@gmail.com>
2018-10-25 01:13:46 -0300

    Cambios

--- src/fachada/Fachada.java
+++ src/fachada/Fachada.java
@@ -388,146 +388,146 @@ public class Fachada{
                + "de primera división, y tras un breve paso por el Örgryte IS, en 2011 se marchó al IFK "
                + "Norrköping,1​ donde llegó su primer título como entrenador con la Allsvenskan y la "
                + "Supercopa de Suecia, lo cual le llevó en la temporada siguiente a entrenar a la selección "
-               + "de fútbol de Suecia.", 5, "Janee Andersson", "Suecia", "src/img/default.jpg");
+               + "de fútbol de Suecia.", 5, "Janee Andersson", "Suecia", "src/img/directores/DirectorSuecia.png");
 
        Director direcRusia = new Director(1, "Antes de su nombramiento como seleccionador de Rusia en "
                + "2016, había entrenado a clubes de Austria, Rusia y Polonia, donde conquistó con el Legia de"
                + " Varsovia el título de liga en la temporada 2"
-               + "015-2016", 55, "CHERCHESOV STANISLAV", "Rusia", "src/img/default.jpg");
+               + "015-2016", 55, "CHERCHESOV STANISLAV", "Rusia", "src/img/directores/DirectorRusia.png");
 
        Director direcSenegal = new Director(2, "Tras colgar las botas, Cisse ingresó en el cuerpo técnico "
                + "de la selección senegalesa. En 2015 sucedió a Alain"
-               + " Giresse en el banquillo.", 42, "Cisse Aliou", "Senegal", "src/img/default.jpg");
+               + " Giresse en el banquillo.", 42, "Cisse Aliou", "Senegal", "src/img/directores/DirectorSenegal.png");
 
        Director direcEgipto = new Director(3, "Llega a Rusia 2018 luego de tres años al frente de los Faraones"
                + ", a los que condujo hasta la final de la Copa Africana de Naciones 2017, y de nuevo a una fase"
-               + " final mundialista tras 28 años de ausencia.", 62, "Cuper Hector", "Egipto", "src/img/default.jpg");
+               + " final mundialista tras 28 años de ausencia.", 62, "Cuper Hector", "Egipto", "src/img/directores/DirectorEgipto.png");
 
        Director direcCroacia = new Director(4, "Tras haber sustituido a Ante Cacic al frente de la selección"
                + " absoluta de Croacia en octubre de 2017, Dalic logró alcanzar la repesca con una victoria"
                + " decisiva en la última jornada y, tras imponerse en el doble enfrentamiento contra Grecia,"
                + " selló la clasificación de Croacia"
-               + " para Rusia 2018.", 51, "Dalic Zalatko", "Croacia", "src/img/default.jpg");
+               + " para Rusia 2018.", 51, "Dalic Zalatko", "Croacia", "src/img/directores/DirectorCroacia.png");
 
        Director direcFrancia = new Director(5, "Didier Deschamps, uno de los jugadores más laureados"
                + " del fútbol francés, dirigió con éxito al Mónaco, a la Juventus y al Olympique de Marsella. "
                + "Fue nombrado seleccionador de Francia tras la Eurocopa 2012: cuartofinalista en el"
                + " Mundial de 2014 y "
-               + "subcampeón en la Eurocopa 2016.", 49, "Deschamps Didier", "Francia", "src/img/default.jpg");
+               + "subcampeón en la Eurocopa 2016.", 49, "Deschamps Didier", "Francia", "src/img/directores/DirectorFrancia.png");
 
        Director direcPeru = new Director(6, "Tras ser un exitoso delantero centro, Ricardo Gareca empezó"
                + " como entrenador en Argentina. Tuvo experiencias en Colombia, Perú y Brasil. Ganó cuatro "
                + "títulos de liga. En marzo de 2015 asumió como DT de la selección peruana, a la que llevó al "
-               + "Mundial tras 36 años de ausencia.", 60, "Gareca Ricardo", "Peru", "src/img/default.jpg");
+               + "Mundial tras 36 años de ausencia.", 60, "Gareca Ricardo", "Peru", "src/img/directores/DirectorPeru.png");
 
-       Director direcPanama = new Director(7, "SIN DESCRIPCION", 62, "Gomez Hernan", "Panama", "src/img/default.jpg");
+       Director direcPanama = new Director(7, "SIN DESCRIPCION", 62, "Gomez Hernan", "Panama", "src/img/directores/DirectorPanama.png");
 
        Director direcIslandia = new Director(8, "Ex dentista de profesión, Lallgrimsson fue nombrado en 2011 "
                + "ayudante del entonces seleccionador de Islandia, Lars Lagerback, y, dos años después, pasó a "
                + "dirigir al equipo conjuntamente con el sueco. Tras la histórica actuación en la Eurocopa 2016,"
                + " asumió en solitario las riendas, clasificándola"
-               + " para su primer Mundial.", 51, "Hallgrimsson Heimir", "islandia", "src/img/default.jpg");
+               + " para su primer Mundial.", 51, "Hallgrimsson Heimir", "islandia", "src/img/directores/DirectorIslandia.png");
 
        Director direcDinamarca = new Director(9, "Age Hareide, un ex defensa con experiencia en la primera "
                + "división inglesa, fue internacional 50 veces con Noruega entre 1976 y 1986. Como entrenador, "
                + "ha ganado títulos de liga en tres países y dirigió a la selección noruega. En Dinamarca sucedió "
-               + "a Morten Olsen a finales de 2015.", 63, "Hareider Age", "Dinamarca", "src/img/default.jpg");
+               + "a Morten Olsen a finales de 2015.", 63, "Hareider Age", "Dinamarca", "src/img/directores/DirectorDinamarca.png");
 
-       Director direcEspania = new Director(10, "SIN DESCRIPCION", 50, "Hierro Fernando", "España", "src/img/default.jpg");
+       Director direcEspania = new Director(10, "SIN DESCRIPCION", 50, "Hierro Fernando", "España", "src/img/directores/DirectorEspaña.png");
 
-       Director direcMexico = new Director(11, "SIN DESCRIPCION", 57, "Juan carlos osorio", "Mexico", "src/img/default.jpg");
+       Director direcMexico = new Director(11, "SIN DESCRIPCION", 57, "Juan carlos osorio", "Mexico", "src/img/directores/DirectorMexico.png");
 
        Director direcSerbia = new Director(12, "Elegido como seleccionador serbio a finales de 2017, Krstajic"
                + " desarrolló una carrera como defensor principalmente en Alemania. Disputó la Copa Mundial "
-               + "de la FIFA Alemania 2006.", 44, "Krstajic MLaden", "Serbia", "src/img/default.jpg");
+               + "de la FIFA Alemania 2006.", 44, "Krstajic MLaden", "Serbia", "src/img/directores/DirectorSerbia.png");
 
        Director direcAlemania = new Director(13, "Bajo la batuta de Loew, Alemania conquistó el Mundial"
                + " en 2014 tras haber quedado tercero en 2010, y ganó la Copa FIFA Confederaciones 2017. "
                + "Asimismo, ha alcanzado una final (2008) y dos semifinales (2012, 2016) en las tres últimas e"
-               + "diciones de la Eurocopa.", 58, "Low Joachim", "Alemania", "src/img/default.jpg");
+               + "diciones de la Eurocopa.", 58, "Low Joachim", "Alemania", "src/img/directores/DirectorAlemania.png");
 
        Director direcTunez = new Director(14, "Nabil Maâloul fue ayudante de Roger Lemerre durante la "
                + "campaña triunfal de Túnez en la Copa Africana de Naciones 2004, disfrutó de un gran éxito a"
                + " su regreso al Esperance de su país y llevó a Kuwait a la Copa Asiática 2015. Es su segunda "
-               + "etapa como seleccionador tunecino.", 56, "Maaloul Nabil", "Tunes","src/img/default.jpg");
+               + "etapa como seleccionador tunecino.", 56, "Maaloul Nabil", "Tunes","src/img/directores/DirectorTunez.png");
 
        Director direcBelgica = new Director(15, "A las órdenes del técnico español, los  _Diablos Rojos_  "
                + "se convirtieron en el primer país de la UEFA en clasificarse para Rusia 2018, y lo hicieron sin"
-               + " conocer la derrota.", 45, "Martinez Roberto", "Belgica", "src/img/default.jpg");
+               + " conocer la derrota.", 45, "Martinez Roberto", "Belgica", "src/img/directores/DirectorBelgica.png");
 
        Director direcPolonia = new Director(16, "Como técnico, ha acumulado experiencia con diferentes "
                + "equipos, primero como asistente y después como primer entrenador. Lleva la batuta de "
-               + "Polonia desde 2013.", 60, "Nawalka Adam", "Polonia","src/img/default.jpg");
+               + "Polonia desde 2013.", 60, "Nawalka Adam", "Polonia","src/img/directores/DirectorPolonia.png");
 
        Director direcJapon = new Director(30, "Este antiguo centrocampista fue 12 veces internacional entre "
                + "1977 y 1978. Tras colgar las botas, entrenó a los combinados nipones de"
-               + " categorías menores.", 63, "NISHINO Akira", "Japon", "src/img/default.jpg");
+               + " categorías menores.", 63, "NISHINO Akira", "Japon", "src/img/directores/DirectorJapon.png");
 
        Director direcColombia = new Director(17, "Bajo su batuta, los _Cafeteros _consiguieron en Brasil "
                + "2014 su mejor resultado hasta la fecha en un Mundial, "
-               + "accediendo a cuartos.", 69, "PEKERMAN Jose", "Colombia", "src/img/default.jpg");
+               + "accediendo a cuartos.", 69, "PEKERMAN Jose", "Colombia", "src/img/directores/DirectorColombia.png");
 
        Director direcSuiza = new Director(18, "Tomó las riendas del equipo nacional luego de Brasil 2014 y,"
                + " hasta la fecha, ha conseguido clasificar al cuadro helvético a la EURO "
-               + "2016 y ahora Rusia 2018", 55, "PETKOVIC Vladimir", "Suiza", "src/img/default.jpg");
+               + "2016 y ahora Rusia 2018", 55, "PETKOVIC Vladimir", "Suiza", "src/img/directores/DirectorSuiza.png");
 
        Director direcArabia = new Director(19, "Pizzi se ha desempeñado como entrenador en Perú, "
                + "Chile, España, México y Argentina. Con 'La Roja' quedó subcampeón de la Copa FIFA "
-               + "Confederaciones 2017.", 50, "PIZZI Juan Antonio", "Arrabia Saudita", "src/img/default.jpg");
+               + "Confederaciones 2017.", 50, "PIZZI Juan Antonio", "Arrabia Saudita", "src/img/directores/DirectorArabia.png");
 
        Director direcIran = new Director(20, "Desde abril de 2011, Queiroz dirige a la selección de Irán, a "
                + "la que clasificó para el Mundial de 2014, para la Copa Asiática 2015 y, "
-               + "ahora, para Rusia 2018.", 65, "QUEIROZ Carlos", "Iran", "src/img/default.jpg");
+               + "ahora, para Rusia 2018.", 65, "QUEIROZ Carlos", "Iran", "src/img/directores/DirectorIran.png");
 
        Director direcCostaRica = new Director(21, "Como jugador de la Sele participó en Italia 1990 y "
                + "completó cuatro fases de clasificación mundialistas. En los banquillos, destaca su paso "
                + "por la Liga Deportiva Alajuelense y los tres años que lleva al frente del equipo tico, a quien"
                + " condujo hasta las semifinales de"
-               + " la Copa Oro 2017", 50, "RAMIREZ Oscar", "Costa Rica", "src/img/default.jpg");
+               + " la Copa Oro 2017", 50, "RAMIREZ Oscar", "Costa Rica", "src/img/directores/DirectorCostarica.png");
 
        Director direcMarruecos = new Director(22, "En febrero de 2016, el francés fue elegido para"
                + " hacerse cargo de la selección de Marruecos y logró clasificar a los norteafricanos para su"
-               + " primer Mundial en 20 años", 50, "RENARD Herve", "Marruecos", "src/img/default.jpg");
+               + " primer Mundial en 20 años", 50, "RENARD Herve", "Marruecos", "src/img/directores/DirectorMarruecos.png");
 
        Director direcNigeria = new Director(23, "El alemán Gernot Rohr tiene basta experiencia como seleccionador"
                + ". De hecho, Nigeria es el cuarto país africano al que conduce. Los anteriores fueron Gabón"
                + ", Níger y Burkina Faso, a la que incluso dirigió en dos sus partidos del clasificatorio para Rusia "
-               + "2018. Asumió el mando de Nigeria en 2016.", 65, "ROHR Gernot", "Nigeria", "src/img/default.jpg");
+               + "2018. Asumió el mando de Nigeria en 2016.", 65, "ROHR Gernot", "Nigeria", "src/img/directores/DirectorNigeria.png");
 
        Director direcArgentina = new Director(24, "En junio de 2017, Sampaoli dimitió como técnico del Sevilla para ser "
                + "nombrado seleccionador de su Argentina natal. Tras haber dirigido ya a Chile en 4 partidos de clasificación"
                + " para Rusia 2018, condujo a la_ Albiceleste_ a"
-               + " clasificarse para ese Mundial", 58, "SAMPAOLI Jorge", "Argentina", "src/img/default.jpg");
+               + " clasificarse para ese Mundial", 58, "SAMPAOLI Jorge", "Argentina", "src/img/directores/DirectorArgentina.png");
 
        Director direcPortugal = new Director(25, "Dirige a Portugal desde septiembre de 2014, y ha guiado"
                + " a los portugueses hasta la victoria en la EURO 2016, además de en la Copa FIFA"
-               + " Confederaciones, donde quedaron terceros.", 64, "SANTOS Fernando", "Portugal", "src/img/default.jpg");
+               + " Confederaciones, donde quedaron terceros.", 64, "SANTOS Fernando", "Portugal", "src/img/directores/DirectorPortugal.png");
 
        Director direcCorea = new Director(26, "En junio de 2017, Shin fue elegido para suceder a Uli Stielike "
                + "en el cargo de seleccionador del"
-               + " combinado nacional absoluto.", 49, "SHIN Taeyong", "Correa", "src/img/default.jpg");
+               + " combinado nacional absoluto.", 49, "SHIN Taeyong", "Correa", "src/img/directores/DirectorCorea.png");
 
        Director direcInglaterra = new Director(27, "Su carrera de entrenador la inició en el Middlesbrough y,"
                + " en 2013, fue nombrado seleccionador nacional sub-21. En septiembre de 2016, asumió la"
-               + " batuta de los Pross.", 48, "SOUTHGATE Gareth", "Inglaerra", "src/img/default.jpg");
+               + " batuta de los Pross.", 48, "SOUTHGATE Gareth", "Inglaerra", "src/img/directores/DirectorInglaterra.png");
 
        Director direcUruguay = new Director(28, "Tras aquel primer ciclo al frente de Uruguay,"
                + " en el que disputó el Mundial de Italia 1990, su segundo paso por la selección ha marcado"
                + " un antes y un después para la Celeste. No sólo ganó una Copa América, sino que se apresta"
                + " a disputar en Rusia su tercer Mundial"
-               + "secutivo.", 71, "TABAREZ Oscar", "Uruguay", "src/img/default.jpg");
+               + "secutivo.", 71, "TABAREZ Oscar", "Uruguay", "src/img/directores/DirectorUruguay.png");
 
        Director direcBrasil = new Director(29, "En junio de 2016, Tite asumió en la selección de Brasil. "
                + "Gracias a una excepcional eliminatoria desde su llegada, el equipo se convirtió en "
                + "el primer país en obtener su billete a Rusia. Antes, sus mayores éxitos en clubes los"
                + " obtuvo durante sus tres etapas distintas al frente del"
-               + " Corinthians.", 57, "TITE", "Brasil", "src/img/default.jpg");
+               + " Corinthians.", 57, "TITE", "Brasil", "src/img/directores/DirectorBrasil.png");
 
        Director direcAustralia = new Director(31, "Van Marwijk dirigió a Países Bajos entre 2008 y 2012, "
                + "quedando  subcampeón en Sudáfrica 2010. En 2015 tomó las riendas de Arabia Saudí y "
                + "clasificó al equipo para Rusia 2018, pero dimitió poco después. En enero de 2018 fue "
                + "designado para entrenar a Au"
-               + "stralia.", 66, "VAN MARWIJK Bert", "Australia", "src/img/default.jpg");
+               + "stralia.", 66, "VAN MARWIJK Bert", "Australia", "src/img/directores/DirectorAustralia.png");
 
        directores.add(direcAlemania);
        directores.add(direcArgentina);
